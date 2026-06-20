97143fb51633a870a4238ac67ff515277dffeeef
jathniel-07 <jjdrums123@gmail.com>
2026-06-20 13:50:04 +0530

    Add Multiplication class with table calculation method

---
+++ Multiplication_table.py
@@ -0,0 +1,8 @@
+class Multiplication:
+    def calc_Multiplication_table(self):
+        x=int(input("Enter a range of multiplication table: "))
+        y=int(input("Enter the multiple to be found: "))
+        for i in range(1,x+1):
+            print("|",i," x ",y," = ",i*y,"|")
+        print(f"The multiplication table of {y} is printed")
+        searching()
